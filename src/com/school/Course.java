c636fa7cb94e5b62c31cf95fb43f9ec9a29c15f5
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-08-11 11:33:07 +0530

    part-04

--- src/com/school/Course.java
+++ src/com/school/Course.java
@@ -1,17 +1,18 @@
+
 package com.school;
 
 public class Course {
+    private static int nextCourseIdCounter = 101; 
     int courseId;
     String courseName;
 
-    // Method to set details
-    public void setDetails(int courseId, String courseName) {
-        this.courseId = courseId;
+    public Course(String courseName) {
+        this.courseId = nextCourseIdCounter++;
         this.courseName = courseName;
     }
 
     public void displayDetails() {
-        System.out.println("Course ID: " + courseId);
+        System.out.println("Course ID: C" + courseId);
         System.out.println("Course Name: " + courseName);
     }
 }
