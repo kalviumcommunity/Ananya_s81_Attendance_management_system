5193f13f666bdc29ceea288c4ebf50640324fadf
AnanyaCherukuri05 <ananyachrukuri5@gmail.com>
2025-08-11 10:51:32 +0530

    Fixed src/ not being a submodule

---
+++ src/com/school/Course.java
@@ -0,0 +1,17 @@
+package com.school;
+
+public class Course {
+    int courseId;
+    String courseName;
+
+    // Method to set details
+    public void setDetails(int courseId, String courseName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+    }
+
+    public void displayDetails() {
+        System.out.println("Course ID: " + courseId);
+        System.out.println("Course Name: " + courseName);
+    }
+}
